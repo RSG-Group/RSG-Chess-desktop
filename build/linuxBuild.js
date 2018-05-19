a869fedaaf5363137381058e696496a8236be671
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2018-05-19 18:37:55 +0530

    Update Electron + electron-installer-linux. Red Hat installer broken. Debian and Snap unconfirmed. Building flatpak freezes the script.

--- build/linuxBuild.js
+++ build/linuxBuild.js
@@ -1,8 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies,no-console */
-const debian = require('electron-installer-linux/debian')
-const redhat = require('electron-installer-linux/redhat')
-const flatpak = require('electron-installer-linux/flatpak')
-const snap = require('electron-installer-snap')
+const { debian, redhat, flatpak, snap } = require('electron-installer-linux')
 
 console.log('If you are generating a flatpak installer, please install flatpak and flatpak-builder.')
 console.log('If you are generating a .rpm installer, please install rpm or rpm-build depending on your distro.')
@@ -26,30 +22,28 @@ const options = {
     '128x128': 'build/icons/128x128.png'
   }
 }
-const flatpakOptions = Object.assign(options, { id: 'RSG-Chess-desktop' })
-const snapOptions = Object.assign(options, {
-  confinement: 'classic',
-  grade: 'stable',
-  description: `RSG Chess stuff.`
-})
-
-const callback = (err) => {
-  if (err) {
-    console.error(err, err.stack)
-    return err
-  }
-  console.log(`Successfully created package at ${options.dest}`)
-  return true
+const flatpakOptions = {
+  id: 'RSG-Chess-desktop',
+  arch, icon: options.icon, categories: options.categories, dest: options.dest, src: options.src
+}
+const snapOptions = {
+  confinement: 'classic', grade: 'stable', description: `RSG Chess stuff.`,
+  arch, icon: options.icon, categories: options.categories, dest: options.dest, src: options.src
 }
 
-if ('debian' in process.argv || 'redhat' in process.argv || 'flatpak' in process.argv) {
+const callback = () => console.log(`Successfully created package at ${options.dest}`)
+const errCallback = (err) => console.error(err, err.stack)
+const args = process.argv
+
+if (args.includes('--build') && (
+  args.includes('redhat') || args.includes('debian') ||
+  args.includes('snap') || args.includes('flatpak')
+)) {
   process.argv.map((arg) => {
-    if (arg === 'debian') debian(options, callback)
-    if (arg === 'redhat') redhat(options, callback)
-    if (arg === 'flatpak') flatpak(flatpakOptions, callback)
-    if (arg === 'snap') {
-      snap(snapOptions)
-    }
+    if (arg === 'debian') debian(options).then(callback).catch(errCallback)
+    if (arg === 'redhat') redhat(options).then(callback).catch(errCallback)
+    if (arg === 'flatpak') flatpak(flatpakOptions).then(callback).catch(errCallback)
+    if (arg === 'snap') snap(snapOptions).then(callback).catch(errCallback)
     return true
   })
 } else {
